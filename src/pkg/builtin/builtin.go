c03c0bd92b721b2150be10ca0738259c05337ca9
Oling Cat <olingcat@gmail.com>
2012-11-03 14:31:51 +0800

    builtin: 优化内建类型的描述。

--- src/pkg/builtin/builtin.go
+++ src/pkg/builtin/builtin.go
@@ -11,114 +11,114 @@
 
 /*
 	builtin 包为Go的预声明标识符提供了文档.
-	此处记录的项目实际上并不在 buildin 包中，但此处对它们的描述允许 godoc
-	为该语言的特殊标识符提供文档。
+	此处列出的条目其实并不在 buildin 包中，对它们的描述只是为了让 godoc
+	给该语言的特殊标识符提供文档。
 */
 package builtin
 
 // bool is the set of boolean values, true and false.
 
-// bool 为布尔值的集合，即 true 和 false。
+// bool 是布尔值的集合，即 true 和 false。
 type bool bool
 
 // uint8 is the set of all unsigned 8-bit integers.
 // Range: 0 through 255.
 
-// uint8 为所有无符号8位整数的集合。
+// uint8 是所有无符号8位整数的集合。
 // 范围：0 至 255。
 type uint8 uint8
 
 // uint16 is the set of all unsigned 16-bit integers.
 // Range: 0 through 65535.
 
-// uint16 为所有无符号16位整数的集合。
+// uint16 是所有无符号16位整数的集合。
 // 范围：0 至 65535。
 type uint16 uint16
 
 // uint32 is the set of all unsigned 32-bit integers.
 // Range: 0 through 4294967295.
 
-// uint32 为所有无符号32位整数的集合。
+// uint32 是所有无符号32位整数的集合。
 // 范围：0 至 4294967295。
 type uint32 uint32
 
 // uint64 is the set of all unsigned 64-bit integers.
 // Range: 0 through 18446744073709551615.
 
-// uint64 为所有无符号64位整数的集合。
+// uint64 是所有无符号64位整数的集合。
 // 范围：0 至 18446744073709551615。
 type uint64 uint64
 
 // int8 is the set of all signed 8-bit integers.
 // Range: -128 through 127.
 
-// int8 为所有带符号8位整数的集合。
+// int8 是所有带符号8位整数的集合。
 // 范围：-128 至 127。
 type int8 int8
 
 // int16 is the set of all signed 16-bit integers.
 // Range: -32768 through 32767.
 
-// int16 为所有带符号16位整数的集合。
+// int16 是所有带符号16位整数的集合。
 // 范围：-32768 至 32767。
 type int16 int16
 
 // int32 is the set of all signed 32-bit integers.
 // Range: -2147483648 through 2147483647.
 
-// int32 为所有带符号32位整数的集合。
+// int32 是所有带符号32位整数的集合。
 // 范围：-32768 至 32767。
 type int32 int32
 
 // int64 is the set of all signed 64-bit integers.
 // Range: -9223372036854775808 through 9223372036854775807.
 
-// int64 为所有带符号64位整数的集合。
+// int64 是所有带符号64位整数的集合。
 // 范围：-9223372036854775808 至 9223372036854775807。
 type int64 int64
 
 // float32 is the set of all IEEE-754 32-bit floating-point numbers.
 
-// float32 为所有IEEE-754 32位浮点数的集合。
+// float32 是所有IEEE-754 32位浮点数的集合。
 type float32 float32
 
 // float64 is the set of all IEEE-754 64-bit floating-point numbers.
 
-// float64 为所有IEEE-754 64位浮点数的集合。
+// float64 是所有IEEE-754 64位浮点数的集合。
 type float64 float64
 
 // complex64 is the set of all complex numbers with float32 real and
 // imaginary parts.
 
-// complex64 为所有带 float32 类型实部和虚部的复数集合。
+// complex64 是所有实部和虚部为 float32 的复数集合。
 type complex64 complex64
 
 // complex128 is the set of all complex numbers with float64 real and
 // imaginary parts.
 
-// complex128 为所有带 float64 类型实部和虚部的复数集合。
+// complex128 是所有实部和虚部为 float64 的复数集合。
 type complex128 complex128
 
 // string is the set of all strings of 8-bit bytes, conventionally but not
 // necessarily representing UTF-8-encoded text. A string may be empty, but
 // not nil. Values of string type are immutable.
 
-// string 为所有8位字节的字符串集合，习惯于但并不必须代表以UTF-8编码的文本。
-// string 可能为空，但不为 nil。string 类型的值是不变的。
+// string 是所有8位字节的字符串集合，习惯上用于代表以UTF-8编码的文本，但并不必须如此。
+// string 可为空，但不为 nil。string 类型的值是不变的。
 type string string
 
 // int is a signed integer type that is at least 32 bits in size. It is a
 // distinct type, however, and not an alias for, say, int32.
 
-// int 为带符号整数类型，其大小至少为32位。
-// 它是一种不同的类型，而不是所谓的 int32 的别名。
+// int 是带符号整数类型，其大小至少为32位。
+// 它是一种确切的类型，而不是 int32 的别名。
 type int int
 
 // uint is an unsigned integer type that is at least 32 bits in size. It is a
 // distinct type, however, and not an alias for, say, uint32.
 
-// uint 为无符号整数类型，其大小至少为32位。
-// 它是一种不同的类型，而不是所谓的 uint32 的别名。
+// uint 是无符号整数类型，其大小至少为32位。
+// 它是一种确切的类型，而不是 uint32 的别名。
 type uint uint
 
 // uintptr is an integer type that is large enough to hold the bit pattern of
@@ -131,14 +131,15 @@ type uintptr uintptr
 // used, by convention, to distinguish byte values from 8-bit unsigned
 // integer values.
 
-// byte 为 uint8 的别名，它在各方面上都等价于 uint8。
-// 它习惯用于区别字节值与8位无符号整数值。
+// byte 为 uint8 的别名，它完全等价于 uint8。
+// 习惯上用它来区别字节值和8位无符号整数值。
 type byte byte
 
 // rune is an alias for int32 and is equivalent int32 in all ways. It is
 // used, by convention, to distinguish character values from integer values.
 
-// rune 为 int32 的别名，它在各方面上都等价于 int32。它习惯用于区别字符值与整数值。
+// rune 为 int32 的别名，它完全等价于 int32。
+// 习惯上用它来区别字符值和整数值。
 type rune rune
 
 // Type is here for the purposes of documentation only. It is a stand-in
@@ -146,7 +147,7 @@ type rune rune
 // invocation.
 
 // Type 在此只用作文档目的。
-// 它是任何Go类型的替身，但对于任何给定的函数调用来说，它都代表与其相同的类型。
+// 它代表所有Go的类型，但对于任何给定的函数请求来说，它都代表与其相同的类型。
 type Type int
 
 // Type1 is here for the purposes of documentation only. It is a stand-in
@@ -154,28 +155,28 @@ type Type int
 // invocation.
 
 // Type1 在此只用作文档目的。
-// 它是任何Go类型的替身，但对于任何给定的函数调用来说，它都代表与其相同的类型。
+// 它代表所有Go的类型，但对于任何给定的函数请求来说，它都代表与其相同的类型。
 type Type1 int
 
 // IntegerType is here for the purposes of documentation only. It is a stand-in
 // for any integer type: int, uint, int8 etc.
 
 // IntegerType 在此只用作文档目的。
-// 它是任何整数类型的替身：如 int、uint、int8 等。
+// 它代表所有的整数类型：如 int、uint、int8 等。
 type IntegerType int
 
 // FloatType is here for the purposes of documentation only. It is a stand-in
 // for either float type: float32 or float64.
 
 // FloatType 在此只用作文档目的。
-// 它是任何浮点数类型的替身：即 float32 或 float64。
+// 它代表所有的浮点数类型：即 float32 或 float64。
 type FloatType float32
 
 // ComplexType is here for the purposes of documentation only. It is a
 // stand-in for either complex type: complex64 or complex128.
 
 // ComplexType 在此只用作文档目的。
-// 它是任何复数类型的替身：即 complex64 或 complex128。
+// 它代表所有的复数类型：即 complex64 或 complex128。
 type ComplexType complex64
 
 // The append built-in function appends elements to the end of a slice. If
@@ -187,7 +188,7 @@ type ComplexType complex64
 //	slice = append(slice, anotherSlice...)
 
 // append 内建函数将元素追加到切片的末尾。
-// 若它有足够的容量，其目标就会重新切片以适应新的元素。否则，就会分配一个新的基本数组。
+// 若它有足够的容量，其目标就会重新切片以容纳新的元素。否则，就会分配一个新的基本数组。
 // append 返回更新后的切片。因此必须存储追加后的结果，通常为包含该切片自身的变量：
 //	slice = append(slice, elem1, elem2)
 //	slice = append(slice, anotherSlice...)
@@ -200,8 +201,8 @@ func append(slice []Type, elems ...Type) []Type
 // len(src) and len(dst).
 
 // copy 内建函数将元素从来源切片复制到目标切片中。
-// （一种特例是，它也能将字节从字符串复制到字节切片中。）来源和目标可以重叠。
-// copy 返回被复制的元素数量，它将是 len(src) 和 len(dst) 中较小的那个。
+// （特殊情况是，它也能将字节从字符串复制到字节切片中）。来源和目标可以重叠。
+// copy 返回被复制的元素数量，它会是 len(src) 和 len(dst) 中较小的那个。
 func copy(dst, src []Type) int
 
 // The delete built-in function deletes the element with the specified key
