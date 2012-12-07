f29634a4682e6100c029fae1b996017c5ebfc995
zhouyangkunta <devnull@localhost>
2012-12-07 17:30:41 +0800

    sort/search.go:修改部分翻译，补全漏掉翻译

--- src/pkg/sort/search.go
+++ src/pkg/sort/search.go
@@ -7,29 +7,30 @@
 
 package sort
 
-// Search 使用二分查找，找出并返回
-// 满足在范围[0,n)内取值为真的最小元素i，假设在范围[0, n)上，
-// f(i) == true 则意味着 f(i+1) == true. 就是说，Search 要求
-// f 函数的取值 对于当i在范围 [0, n)中的一部分（或没有）取值为false
-// 则剩下的一部分为true; Search返回
-// 第一个取值为真的索引. 如果该索引不存在，Search 返回 n
-// (注意，“没找到”返回不是-1，这与strings.Index中的行为
-// 不相同)
+// Search uses binary search to find and return the smallest index i
+// in [0, n) at which f(i) is true, assuming that on the range [0, n),
+// f(i) == true implies f(i+1) == true.  That is, Search requires that
+// f is false for some (possibly empty) prefix of the input range [0, n)
+// and then true for the (possibly empty) remainder; Search returns
+// the first true index.  If there is no such index, Search returns n.
+// (Note that the "not found" return value is not -1 as in, for instance,
+// strings.Index).
 // Search calls f(i) only for i in the range [0, n).
-// Search 调用f(i),i将在范围[0,n)内取值
 //
-// Search常见的用法是在一个可通过索引访问并排好序的数组或切片中
-// 寻找某值x的索引i，
-// 在这种情况下，参数f，或者说是一个闭包，必须指出要搜索的值，以及被搜索的数据
-// 是如何被索引和排序的
+// A common use of Search is to find the index i for a value x in
+// a sorted, indexable data structure such as an array or slice.
+// In this case, the argument f, typically a closure, captures the value
+// to be searched for, and how the data structure is indexed and
+// ordered.
 //
-// 举个例子, 给定一个升序排列的切片数据,
-// 调用 Search(len(data), func(i int) bool { return data[i] >= 23 })
-// 返回满足 data[i] >= 23 的最小索引i. 如果调用者
-// 想知道23是否在切片中, 必须单独判定 data[i] == 23
+// For instance, given a slice data sorted in ascending order,
+// the call Search(len(data), func(i int) bool { return data[i] >= 23 })
+// returns the smallest index i such that data[i] >= 23.  If the caller
+// wants to find whether 23 is in the slice, it must test data[i] == 23
+// separately.
 //
-// 搜索降序排列的数据需要用 <= 操作符，
-// 而不是>=操作符
+// Searching data sorted in descending order would use the <=
+// operator instead of the >= operator.
 //
 // To complete the example above, the following code tries to find the value
 // x in an integer slice data sorted in ascending order:
@@ -41,6 +42,54 @@ package sort
 //	} else {
 //		// x is not present in data,
 //		// but i is the index where it would be inserted.
+//	}
+//
+// As a more whimsical example, this program guesses your number:
+//
+//	func GuessingGame() {
+//		var s string
+//		fmt.Printf("Pick an integer from 0 to 100.\n")
+//		answer := sort.Search(100, func(i int) bool {
+//			fmt.Printf("Is your number <= %d? ", i)
+//			fmt.Scanf("%s", &s)
+//			return s != "" && s[0] == 'y'
+//		})
+//		fmt.Printf("Your number is %d.\n", answer)
+//	}
+//
+
+// Search 使用二分查找，找出并返回
+// 满足在范围[0,n)内，f取值为真的最小索引i，如果在范围[0, n)上，
+// f(i) == true 成立则 f(i+1) == true 也成立. 就是说，Search 要求
+// f 函数的取值 对于当i在范围 [0, n)中的前某一部分序列（或不存在）取值为false时
+// 对范围剩下的一部分取值为true; Search返回
+// 第一个取值为真的索引. 如果该索引不存在，Search 返回 n
+// (注意，“没找到”返回不是-1，这与strings.Index中的行为
+// 不相同)
+// Search 反复调用f(i),i将在范围[0,n)内取值
+//
+// Search常见的用法是在一个可通过索引访问并排好序的数组或切片中
+// 寻找某值x的索引i，
+// 在这种情况下，参数f，或者说是一个闭包，调用需要传入被搜索的值，以及被索引和
+// 排序的数据
+//
+// 举个例子, 给定一个升序排列的切片数据,
+// 调用 Search(len(data), func(i int) bool { return data[i] >= 23 })
+// 返回满足 data[i] >= 23 的最小索引i. 如果调用者
+// 想知道23是否在切片中, 必须另外通过data[i] == 23判断
+//
+// 搜索降序排列的数据需要用 <= 操作符，
+// 而不是>=操作符
+//
+// 补全上面的例子, 下面的代码试图在升序排列的切片中找出整形变量x的索引
+//
+//	x := 23
+//	i := sort.Search(len(data), func(i int) bool { return data[i] >= x })
+//	if i < len(data) && data[i] == x {
+//		// x 在 data[i] 中 
+//	} else {
+//		// x 不在data中
+//		// 但i可以作为x的插入索引
 //	}
 //
 // 一个有趣的例子，猜你想要的数字
