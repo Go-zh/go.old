875c6f166ff97d3bbbfaeff63bd89d935eedc6cb
Oling Cat <olingcat@gmail.com>
2013-02-13 21:39:41 +0800

    doc/codewalk/sharemem: finish; fix some terms.

--- doc/codewalk/urlpoll.go
+++ doc/codewalk/urlpoll.go
@@ -11,10 +11,10 @@ import (
 )
 
 const (
-	numPollers     = 2                // number of Poller goroutines to launch
-	pollInterval   = 60 * time.Second // how often to poll each URL
-	statusInterval = 10 * time.Second // how often to log status to stdout
-	errTimeout     = 10 * time.Second // back-off timeout on error
+	numPollers     = 2                // number of Poller goroutines to launch // Poller Go程的启动数
+	pollInterval   = 60 * time.Second // how often to poll each URL            // 轮询每一个URL的频率
+	statusInterval = 10 * time.Second // how often to log status to stdout     // 将状态记录到标准输出的频率
+	errTimeout     = 10 * time.Second // back-off timeout on error             // 回退超时的错误
 )
 
 var urls = []string{
@@ -24,6 +24,8 @@ var urls = []string{
 }
 
 // State represents the last-known state of a URL.
+
+// State 表示一个URL最后的已知状态。
 type State struct {
 	url    string
 	status string
@@ -32,6 +34,9 @@ type State struct {
 // StateMonitor maintains a map that stores the state of the URLs being
 // polled, and prints the current state every updateInterval nanoseconds.
 // It returns a chan State to which resource state should be sent.
+
+// StateMonitor 维护了一个映射，它存储了URL被轮询的状态，并每隔 updateInterval
+// 纳秒打印出其当前的状态。它向资源状态的接收者返回一个 chan State。
 func StateMonitor(updateInterval time.Duration) chan<- State {
 	updates := make(chan State)
 	urlStatus := make(map[string]string)
@@ -50,6 +55,8 @@ func StateMonitor(updateInterval time.Duration) chan<- State {
 }
 
 // logState prints a state map.
+
+// logState 打印出一个状态映射。
 func logState(s map[string]string) {
 	log.Println("Current state:")
 	for k, v := range s {
@@ -58,6 +65,8 @@ func logState(s map[string]string) {
 }
 
 // Resource represents an HTTP URL to be polled by this program.
+
+// Resource 表示一个被此程序轮询的HTTP URL。
 type Resource struct {
 	url      string
 	errCount int
@@ -65,6 +74,8 @@ type Resource struct {
 
 // Poll executes an HTTP HEAD request for url
 // and returns the HTTP status string or an error string.
+
+// Poll 为 url 执行一个HTTP HEAD请求，并返回HTTP的状态字符串或一个错误字符串。
 func (r *Resource) Poll() string {
 	resp, err := http.Head(r.url)
 	if err != nil {
@@ -78,6 +89,8 @@ func (r *Resource) Poll() string {
 
 // Sleep sleeps for an appropriate interval (dependant on error state)
 // before sending the Resource to done.
+
+// Sleep 在将 Resource 发送到 done 之前休眠一段适当的时间（取决于错误状态）。
 func (r *Resource) Sleep(done chan<- *Resource) {
 	time.Sleep(pollInterval + errTimeout*time.Duration(r.errCount))
 	done <- r
@@ -93,17 +106,21 @@ func Poller(in <-chan *Resource, out chan<- *Resource, status chan<- State) {
 
 func main() {
 	// Create our input and output channels.
+	// 创建我们的输入和输出信道。
 	pending, complete := make(chan *Resource), make(chan *Resource)
 
 	// Launch the StateMonitor.
+	// 启动 StateMonitor。
 	status := StateMonitor(statusInterval)
 
 	// Launch some Poller goroutines.
+	// 启动一些 Poller Go程。
 	for i := 0; i < numPollers; i++ {
 		go Poller(pending, complete, status)
 	}
 
 	// Send some Resources to the pending queue.
+	// 将一些 Resource 发送至 pending 序列。
 	go func() {
 		for _, url := range urls {
 			pending <- &Resource{url: url}
